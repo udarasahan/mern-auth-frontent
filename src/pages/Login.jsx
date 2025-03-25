188b982df16d9f5b53f4b75268b0114d6eda755f
udaraalgonebula <udarasahanj@gmail.com>
2025-03-25 11:45:16 +0530

    working on login and register functions

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom';
+import { data, useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
   const navigate =  useNavigate();
 
+  const { backendUrl, setIsLogin } = useContext(AppContext);
+
   const [state, setState] = useState("Sign Up");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      axios.defaults.withCredentials = true; // For sent cookies to the server
+
+      if (state === "Sign Up") {
+        const data = await axios.post(backendUrl + '/api/auth/register', {name, email, password})
+
+        if(data.success) {
+          setIsLogin(true)
+          navigate('/')
+        }
+      }else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+       toast.error(error.response.data.message)
+    }
+  }
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
@@ -18,7 +44,7 @@ const Login = () => {
         <h2 className='text-3xl font font-semibold text-white text-center mb-3'>{state === "Sign Up" ? 'Create Account' : 'Login'}</h2>
         <p className='text-center text-sm mb-6'>{state === "Sign Up" ? 'Create Your Account' : 'Login to Your Account!'}</p>
 
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {state === "Sign Up" && (
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.person_icon} alt="" />
